ac7f9eeb34023fb827421333e7618035f69a0804
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-27 23:41:42 +0000

    Get list of most visited cities

---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,7 @@
+SELECT 
+properties.city, 
+COUNT(reservations) as total_reservations
+FROM reservations
+JOIN properties ON properties.id = property_id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
